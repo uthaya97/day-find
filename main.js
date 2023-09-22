ebcfe98e6d417f34c6a3a1dfd3793caaec707228
paulkani <paulkani.s@golden-hippo.com>
2023-09-22 16:19:51 +0530

    day

---
+++ main.js
@@ -0,0 +1,26 @@
+var num=parseInt(prompt("Enter number"))
+
+if(num==1){
+    console.log("Sunday");
+}
+if(num==2){
+    console.log("Monday");
+}
+if(num==3){
+    console.log("Tuesday");
+}
+if(num==4){
+    console.log("Wednesday");
+}
+if(num==5){
+    console.log("Thursday");
+}
+if(num==6){
+    console.log("Friday");
+}
+if(num==7){
+    console.log("Satuerday");
+}
+else{
+    console.log("please enter valid number");
+}
